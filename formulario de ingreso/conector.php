31ca71a22287dbd52c547ca23e428af33e7a7133
Carlos Blockeado <45248351+Cuabu@users.noreply.github.com>
2024-02-22 23:36:52 -0500

    php

---
+++ formulario de ingreso/conector.php
@@ -0,0 +1,20 @@
+<?php
+// Datos de conexión a la base de datos
+$servername = "localhost"; // Cambia esto si tu servidor de MySQL está en otro lugar
+$username = "root"; // Cambia por tu nombre de usuario de MySQL
+$password = "123456"; // Cambia por tu contraseña de MySQL
+$database = "speed_store_desarrollo"; // Nombre de la base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa";
+}
+
+// Cerrar conexión
+$conn->close();
+?>
